420262ba2bfb2daccb9c8815dd4bf76938d2719a
yimaiyan <1753695086@qq.com>
2022-01-23 18:36:49 +0800

    [Yimai]chore: update doomfist line

--- static/index.js
+++ static/index.js
@@ -341,10 +341,10 @@ function shareText(score) {
         return '倒计时多了' + ((deviation_time / 1000) - 20).toFixed(2) + "s";
     }
     SubmitResults();
-    if (score <= 49) return '试着好好练一下？';
-    if (score <= 99) return 'TCL';
-    if (score <= 149) return 'TQL';
-    if (score <= 199) return '您';
+    if (score <= 49) return '末日铁拳，你必将被绳之以法。';
+    if (score <= 99) return 'K.O.';
+    if (score <= 149) return '一拳下去，你可能会死';
+    if (score <= 199) return '毁天灭地';
     return '人？';
 }
 
